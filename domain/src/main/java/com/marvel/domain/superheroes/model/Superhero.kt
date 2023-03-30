f1eec0bcd2a4c06369c4df86b134f9f55585f5c3
Juan Carlos Garrote <juanca_arrabal@hotmail.com>
2023-03-30 19:24:40 +0200

    Created superhero domain model class

---
+++ domain/src/main/java/com/marvel/domain/superheroes/model/Superhero.kt
@@ -0,0 +1,11 @@
+/**
+ * @author Juan Carlos Garrote Gascón, 2023
+ */
+
+package com.marvel.domain.superheroes.model
+
+data class Superhero(
+    val name: String,
+    val description: String,
+    val imageUrl: String,
+)
